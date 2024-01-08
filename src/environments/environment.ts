0e48847202beb12a3171af141d4a704a87f26206
Exaud05 <snopyyoussef@gmail.com>
2024-01-08 08:50:44 +0100

    commit

---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  firebase: {
+    projectId: 'objets-connectes-projet',
+    appId: '1:589179458477:web:c0fccef495efcba09e4724',
+    databaseURL: 'https://objets-connectes-projet-default-rtdb.europe-west1.firebasedatabase.app',
+    storageBucket: 'objets-connectes-projet.appspot.com',
+    apiKey: 'AIzaSyAwFPXtDPzENw0t65rSnKAwoTg2qXPTsHM',
+    authDomain: 'objets-connectes-projet.firebaseapp.com',
+    messagingSenderId: '589179458477',
+  },
+  production: false
+};
